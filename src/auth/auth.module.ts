33ab026d5e7e54c0bb422cbad814bf3bc83f1466
Leandro Oliveira <leandro.oliveira@otisoftware.com.br>
2026-04-18 21:48:35 -0300

    wip: cria module e service e inicia controller

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller.js';
+import { AuthService } from './auth.service.js';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      global: true,
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SUPER_SECRET'),
+        signOptions: { expiresIn: '30s' },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private configService: ConfigService,
+    private jwtService: JwtService,
+  ) {}
+  async signIn(username: string, password: string): Promise<any> {
+    const adminUser = this.configService.get<string>('ADMIN_EMAIL');
+    const adminPasswordHash = this.configService.get<string>(
+      'ADMIN_PASSWORD_HASH',
+    );
+
+    if (!adminPasswordHash || !adminUser) {
+      throw new Error('Variáveis de ambiente não configuradas');
+    }
+
+    const isValid = await bcrypt.compare(password, adminPasswordHash);
+
+    if (username !== adminUser || !isValid) {
+      throw new UnauthorizedException('Credenciais inválidas');
+    }
+
+    const payload = { username: username };
+
+    return await this.jwtService.signAsync(payload);
+  }
+}
